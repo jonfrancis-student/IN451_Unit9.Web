d369ec926dde3ffa8f2f6803c96a26c09241f2b6
Jon Francis <93454931+jonfrancis-Dev@users.noreply.github.com>
2025-05-06 14:49:52 -0400

    Add project files.

---
+++ IN451_Unit9.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IN451_Unit9.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
